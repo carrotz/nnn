2ec971913324b8f76175d9fb2a822ba74380ecdf
carrotzhou@live.com <carrotz@CarrotZdeMacBook-Pro.local>
2016-11-23 15:43:19 +0800

    update whattocook sql

--- WhatToCook/APP/Common/Conf/config.php
+++ WhatToCook/APP/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql', //数据库类型
-    'DB_HOST'=>'139.198.3.221', //服务器地址
+    'DB_HOST'=>'localhost', //服务器地址
     'DB_NAME'=>'whattocook', //数据库名
     'DB_USER'=>'root', //用户名
-    'DB_PWD'=>'123456', //密码
+    'DB_PWD'=>'19910201', //密码
     'DB_PORT'=>'3306', //端口
     'DB_PREFIX'=>'', //数据库表前缀
     
